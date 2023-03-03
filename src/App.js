54223613ca0b90e1454ccc8794c75481a4186d99
tmitusis <tmitusis@gmail.com>
2023-03-03 13:00:13 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Paper } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Paper elevation={4}>Moodle test generator</Paper>
+    </div>
+  );
+}
+
+export default App;
