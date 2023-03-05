74798f36e15489229dd94db2891445c4734b909c
tmitusis <tmitusis@gmail.com>
2023-03-05 15:54:06 +0200

    Fix module loading code

--- js/main.js
+++ js/main.js
@@ -2,22 +2,59 @@
 const to_export = (function (){
     $(document).ready(function () {
         const el = $('#question-selection');
+        const body_ref = $(document.body);
         
         el.on('change', function () {
             const mod = el.val();
-    
+            const mod_name = $('#question-selection option:selected').text();
+
             // Remove all the previously loaded modules
             $('script.module').remove();
-    
+
             // Load the selected module
-            $(document.head).append(`<script id="${mod}" class="module" type="application/javascript" src="js/modules/${mod}/index.js"></script>`);
+            // Using this instead of ES6 imports because they are static.
+            // This way is more dynamic and user friendly
+            $.getScript( `js/modules/${mod}/index.js`).done(function (script, textStatus) {
+                $('#module-root-wrapper').remove();
+
+                body_ref.append('<div class="row" id="module-root-wrapper"></div>');
+
+                // On successful load init the module with it's parent
+                init($('#module-root-wrapper'));
+            }).fail(function (e, jqxhr, settings, exception) {
+                const prev_el = $('#failed-loading-alert');
+
+                if (prev_el.length) {
+                    prev_el.remove();
+                }
+
+                // Alert handling code
+                body_ref.append(
+                    `<div id="failed-loading-alert" style="margin-top: 40px" class="row"><div class="offset-4 col-4 alert alert-warning module-failed-loading" role="alert">
+                      [${jqxhr.toUpperCase()}] Script "${mod_name}" failed to load: "${e.statusText}"
+                    </div></div>`
+                );
+
+                const alert_el = $(`#failed-loading-alert`);
+
+                const alert_timeout_id = setTimeout(function () {
+                    const fadeout_ts = 400;
+
+                    alert_el.fadeOut(fadeout_ts, alert_el.remove.bind(alert_el));
+                }, 5000);
+
+                alert_el.on('click', function () {
+                    alert_el.remove();
+                    clearTimeout(alert_timeout_id);
+                });
+            });
         });
     });
     
     
     function _compressionUpdate(obj) {
-        console.log(`Progressed ${obj.percent}%`);
-    
+        console.log(`--> Progressed ${obj.percent}%`);
+
         if (obj.currentFile) {
             console.log(`\tCurrent file is: ${obj.currentFile}`);
         }
@@ -61,19 +98,24 @@ const to_export = (function (){
         }
     }
     
-    function saveFile(fblob, fname) {
-        if (typeof fname !== 'stirng') {
+    function saveFile(data, fname) {
+        if (typeof fname !== 'string') {
             fname = 'moodle-question-bank.zip';
         }
-    
+
         window.URL = window.URL || window.webkitURL;
-        
-        //http://html5-demos.appspot.com/static/a.download.html
-        //https://developer.mozilla.org/en-US/docs/Web/API/Blob
-        var a = document.createElement('a');
-        a.download = fname;
-        a.href = window.URL.createObjectURL(fblob);
-        a.click();
+
+        generateZip(data, function (err, fblob) {
+            if (err) {
+                alert('Failed to generate ZIP file: ' + err.message);
+                return;
+            }
+
+            const a = document.createElement('a');
+            a.download = fname;
+            a.href = window.URL.createObjectURL(fblob);
+            a.click();
+        });
     }
 
     return {
@@ -82,5 +124,8 @@ const to_export = (function (){
     };
 })();
 
+// Exporting variables to global scope
 const generateZip = to_export.generateZip;
+
+// Generally this should be used
 const saveFile = to_export.saveFile;
