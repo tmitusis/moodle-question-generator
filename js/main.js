265d78a87264b030c3c870d4874348138f093151
tmitusis <tmitusis@gmail.com>
2023-03-05 16:45:42 +0200

    Added utils, replaced messages
    Съобщенията за потребителят вече са
    на български.
    Добавих няколко помощни функции
    за генериране на произволни данни

--- js/main.js
+++ js/main.js
@@ -1,5 +1,210 @@
 // IIFE - does not clutter the global namespace
 const to_export = (function (){
+    var last_el = null;
+
+    const words = [
+        'абаджия',
+        'абажур',
+        'абанос',
+        'абат',
+        'абдал',
+        'абдуктор',
+        'абзац',
+        'абисинец',
+        'абитуриент',
+        'аблаут',
+        'аболиционизъм',
+        'абонамент',
+        'абонат',
+        'абордаж',
+        'абориген',
+        'аборт',
+        'абразив',
+        'абсент',
+        'абсолвент',
+        'абсолют',
+        'абсолютизъм',
+        'абсорбент',
+        'абсорбер',
+        'абстракционист',
+        'абсурд',
+        'абсцес',
+        'авангард',
+        'аванс',
+        'авантаджия',
+        'авантаж',
+        'авантюрист',
+        'авджия',
+        'авиатор',
+        'авиодиспечер',
+        'авиомодел',
+        'авиомотор',
+        'авоар',
+        'австралопитек',
+        'автобус',
+        'автогараж',
+        'автограф',
+        'автозавод',
+        'автоклав',
+        'автократ',
+        'автомат',
+        'автомобил',
+        'автомонтьор',
+        'автопилот',
+        'автопортрет',
+        'автор',
+        'авторемонт',
+        'авторитет',
+        'автохтон',
+        'ага',
+        'агент',
+        'агитатор',
+        'агитпропчик',
+        'агнец',
+        'агрегат',
+        'агресор',
+        'агроном',
+        'ад',
+        'адаптер',
+        'адвокат',
+        'адепт',
+        'адет',
+        'аджамия',
+        'администратор',
+        'адмирал',
+        'адреналин',
+        'адрес',
+        'адресант',
+        'адресат',
+        'аеродрум',
+        'аероплан',
+        'аеротранспорт',
+        'ажур',
+        'азбест',
+        'азиатец',
+        'азимут',
+        'азот',
+        'айгър',
+        'айляк',
+        'айрян',
+        'академизъм',
+        'академик',
+        'акваланг',
+        'аквариум',
+        'акомпанимент',
+        'акорд',
+        'акордеон',
+        'акран',
+        'акредитив',
+        'акробат',
+        'акропол',
+        'аксесоар',
+        'акт',
+        'актив',
+        'активист',
+        'актьор',
+        'акумулатор',
+        'акцент',
+        'акцидент',
+        'акциз',
+        'акционер',
+        'акъл',
+        'алабаш',
+        'албатрос',
+        'албинос',
+        'албумин',
+        'алдехид',
+        'алигатор',
+        'алкохол',
+        'алкохолизъм',
+        'алкохолик',
+        'Аллах',
+        'алманах',
+        'алпинеум',
+        'алпинизъм',
+        'алпинист',
+        'алтруизъм',
+        'алтруист',
+        'алуминий',
+        'алхимик',
+        'аматьор',
+        'амбалаж',
+        'амбреаж',
+        'аметист',
+        'амоний',
+        'амоняк',
+        'амортизатор',
+        'ампер',
+        'амперметър',
+        'амулет',
+        'амфет',
+        'анализ',
+        'аналог',
+        'ананас',
+        'анархизъм',
+        'анасон',
+        'анахронизъм',
+        'ангажимент',
+        'ангел',
+        'анекдот',
+        'анонс',
+        'ансамбъл',
+        'антагонист',
+        'антиквар',
+        'антипод',
+        'антисемитизъм',
+        'антихрист',
+        'антракс',
+        'антракт',
+        'антрацит',
+        'антураж',
+        'анус',
+        'анцуг',
+        'анюитет',
+        'аорист',
+        'апарат',
+        'апартамент',
+        'апатит',
+        'апаш',
+        'апел',
+        'аперитив',
+        'апетит',
+        'аплет',
+        'апломб',
+        'апогей',
+        'апокалипсис',
+        'апологет',
+        'апорт',
+        'апостол',
+        'апостроф',
+        'апотеоз',
+        'аптекар',
+        'арабаджия',
+        'аралък',
+        'арапин',
+        'арбалет',
+        'арбитраж',
+        'арбитър',
+        'аргатин',
+        'аргумент',
+        'арест',
+        'арестант',
+        'аристократ',
+        'аркадаш',
+        'армаган',
+        'арнаутин',
+        'аромат',
+        'арсен',
+        'арсенал',
+        'артикул',
+        'артист',
+        'артък',
+        'архаизъм',
+        'архангел',
+        'архивар',
+        'архиватор'
+    ];
+
     $(document).ready(function () {
         const el = $('#question-selection');
         const body_ref = $(document.body);
@@ -11,16 +216,24 @@ const to_export = (function (){
             // Remove all the previously loaded modules
             $('script.module').remove();
 
+            // Remove the previous module body, if any
+            if (last_el !== null) {
+                $(last_el).remove();
+            }
+
+            // If the default option was selected then don`t try to load it
+            if (mod === 'default') {
+                return;
+            }
+
             // Load the selected module
             // Using this instead of ES6 imports because they are static.
             // This way is more dynamic and user friendly
             $.getScript( `js/modules/${mod}/index.js`).done(function (script, textStatus) {
-                $('#module-root-wrapper').remove();
-
-                body_ref.append('<div class="row" id="module-root-wrapper"></div>');
-
                 // On successful load init the module with it's parent
-                init($('#module-root-wrapper'));
+                init(function (el) {
+                    $('#module-root-wrapper').append(el);
+                });
             }).fail(function (e, jqxhr, settings, exception) {
                 const prev_el = $('#failed-loading-alert');
 
@@ -31,7 +244,7 @@ const to_export = (function (){
                 // Alert handling code
                 body_ref.append(
                     `<div id="failed-loading-alert" style="margin-top: 40px" class="row"><div class="offset-4 col-4 alert alert-warning module-failed-loading" role="alert">
-                      [${jqxhr.toUpperCase()}] Script "${mod_name}" failed to load: "${e.statusText}"
+                      [${jqxhr.toUpperCase()}] Скипр "${mod_name}" не успя да се зареди: "${e.statusText}"
                     </div></div>`
                 );
 
@@ -53,10 +266,10 @@ const to_export = (function (){
     
     
     function _compressionUpdate(obj) {
-        console.log(`--> Progressed ${obj.percent}%`);
+        console.log(`--> Прогрес ${obj.percent}%`);
 
         if (obj.currentFile) {
-            console.log(`\tCurrent file is: ${obj.currentFile}`);
+            console.log(`\tТекущият файл е: ${obj.currentFile}`);
         }
     }
     
@@ -76,7 +289,7 @@ const to_export = (function (){
         const zip = new JSZip();
         const zip_opts = {
             type : "blob",
-            comment: "Moodle question bank automatically generated via Moodle question generator. See https://github.com/tmitusis/moodle-question-generator",
+            comment: "Генератор на въпроси за Moodle. Вижте https://github.com/tmitusis/moodle-question-generator",
             compression: 'DEFLATE',
             compressionOptions: {
                 level: 9
@@ -107,7 +320,7 @@ const to_export = (function (){
 
         generateZip(data, function (err, fblob) {
             if (err) {
-                alert('Failed to generate ZIP file: ' + err.message);
+                alert('Създаването на архив пропадна: ' + err.message);
                 return;
             }
 
@@ -118,9 +331,28 @@ const to_export = (function (){
         });
     }
 
+    function generateRandomInteger(from, to) {
+        return Math.round(
+            (Math.random() * to) + from
+        );
+    }
+
+    function generateRandomFloat(from, to) {
+        return (Math.random() * to) + from;
+    }
+
+    function getRandomNoun() {
+        return words[generateRandomInteger(0, words.length - 1)];
+    }
+
     return {
         generateZip,
-        saveFile
+        saveFile,
+        utils: Object.freeze({
+            generateRandomInteger,
+            generateRandomFloat,
+            getRandomNoun
+        })
     };
 })();
 
@@ -129,3 +361,6 @@ const generateZip = to_export.generateZip;
 
 // Generally this should be used
 const saveFile = to_export.saveFile;
+
+// Random utility functions
+const utils = to_export.utils;
