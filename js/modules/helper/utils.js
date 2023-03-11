bb9846ca4de77b30c0b9deb9ced08d945a24af3a
tmitusis <tmitusis@gmail.com>
2023-03-11 15:35:39 +0200

    helper::utils: Document functions
    Документирах помощните функции

--- js/modules/helper/utils.js
+++ js/modules/helper/utils.js
@@ -1,4 +1,10 @@
 const utils = (function () {
+    /**
+     * @desc Генерира произволно цяло число в обхват, включително
+     * @param {Number} from - начало на обхвата
+     * @param {Number} to - край на обхвата
+     * @returns {number} - генерираното число
+     */
     function generateRandomInteger(from, to) {
         return Math.round(
             (Math.random() * (to - from)) + from
@@ -6,16 +12,34 @@ const utils = (function () {
     }
 
     const INT_RANGE_REGEX = /\[-?\d+\.\.-?\d+\]/gi;
+
+    /**
+     * @desc Генерира произволно число, като приема текст, описващ обхвата
+     * @param {String} str - текст, описващ обхвата. пр: [1..4].
+     * @returns {number} - генерираното число
+     */
     function generateRandomIntegerFromString(str) {
         const range = str.replace(/[\[\]]/g, '').split('..')
         return generateRandomInteger(parseInt(range[0]), parseInt(range[1]));
     }
 
+    /**
+     * @desc Генерира произволно десетично число, в даден обхват, с дадени брой цифри след десетичната запетая
+     * @param {Number} from - начало на обхвата, десетично число
+     * @param {Number} to - край на обхвата, десетично число
+     * @param {Number} digits - брой цифри след десетичната запетая, цяло число
+     * @returns {number} - генерираното число
+     */
     function generateRandomFloat(from, to, digits) {
         return parseFloat(((Math.random() * (to - from)) + from).toFixed(digits));
     }
 
     const FLOAT_RANGE_REGEX = /\[-?(\d+(\.\d+))\.\.-?(\d+(\.\d+))\](@\d+)?/gi;
+    /**
+     * @desc Генерира произволно десетично число, като приема текст, описващ обхвата
+     * @param {String} str - текст, описващ обхвата и точността. пр: [1.0..4.5]@3.
+     * @returns {number} - генерираното число
+     */
     function generateRandomFloatFromString(str) {
         const at_idx = str.indexOf('@');
         const range = str.replace(/[\[\]]/g, '')
@@ -32,16 +56,31 @@ const utils = (function () {
     }
 
     const RANDOM_NOUN_REGEX = /\(съществително\)/gi;
+
+    /**
+     * @desc Връща произволна дума от глобалният речник със съществителни "nouns"
+     * @returns {String} - дсадената дума
+     */
     function getRandomNoun() {
         return nouns[generateRandomInteger(0, nouns.length - 1)];
     }
 
     const RANDOM_ADJECTIVE_REGEX = /\(прилагателно\)/gi;
+    /**
+     * @desc Връща произволна дума от глобалният речник със прилагателни "adjectives"
+     * @returns {String} - дсадената дума
+     */
     function getRandomAdjective() {
         return adjectives[generateRandomInteger(0, adjectives.length - 1)];
     }
 
     const ONEOF_WORD_REGEX = /{([а-я]*[a-z]*)(,[а-я]*[a-z]*)*}/gi;
+
+    /**
+     * @desc Избира една от изброените думи и я връща
+     * @param {String} str - изброените думи. пр: {куче,котка,къща}
+     * @returns {String} - една от горе-изброените думи
+     */
     function getOneOfWord(str) {
         const words = str.replace('{', '')
             .replace('}', '')
--- js/modules/text/index.js
+++ js/modules/text/index.js
@@ -1,1 +1,100 @@
-alert('Text module loaded!');
+function init(appendToBody) {
+    const el_txt = `<form>
+            <div class="row">
+                <label for="category" class="form-label">Категория</label>
+                <input id="category" type="text" placeholder="/примерни/категории" class="form-control" aria-describedby="category_help">
+                <div id="category_help" class="form-text">Категория в която да е въпроса.</div>
+            </div>
+            <div class="row">
+                <label for="number_of_questions" class="form-label">Брой въпроси</label>
+                <input id="number_of_questions" type="number" class="form-control" aria-describedby="number_of_questions_help">
+                <div id="number_of_questions_help" class="form-text">Колко въпроси искате да има</div>
+            </div>
+            <div class="row">
+                <label for="question" class="form-label">Въпрос</label>
+                <textarea id="question" rows="7" class="form-control" aria-describedby="question_help">
+Могат да се използват различен синтаксис за генериране на произволни данни:
+    - едно от: {кучето,къщата,дървото} - ще заменя този текст с един от изброените
+    - в обхват: [1..5] - ще генерирам произволно цяло число между 1 и 5, включително
+    - в обхват: [1.0..5.0] - ще генерирам произволно десетично число между 1.0 и 5.0, включително
+    - в обхват: [1.0..5.0]@3 - ще генерирам произволно десетично число между 1.0 и 5.0, включително до уточненият брой символи(@3) след десетичната запетая. По подразбиране е 15.
+    - използване на вграден речник: Използвайте (съществително) или (прилагателно) и ще избера една дума от съответният тип и ще я вмъкна там
+</textarea>
+                <div id="question_help" class="form-text">Въпросът към изпитваните</div>
+            </div>
+            <button id="generate_questions" type="button" class="btn btn-primary">Генерирай въпроси</button>
+        </form>`
+    ;
+
+    appendToBody($(el_txt));
+
+    $('#generate_questions').click(function () {
+        var question = $('#question').val();
+        const questions = [];
+
+        const int_range = question.match(utils.INT_RANGE_REGEX);
+        const float_range = question.match(utils.FLOAT_RANGE_REGEX);
+        const words = question.match(utils.ONEOF_WORD_REGEX);
+        const nouns = question.match(utils.RANDOM_NOUN_REGEX);
+        const adjectives = question.match(utils.RANDOM_ADJECTIVE_REGEX);
+
+        const q_num = parseInt($('#number_of_questions').val());
+
+        for (let j = 0; j < q_num; ++j) {
+            // Заменя макрото за конкретни думи, колкото и пъти да го има
+            for (let i = 0, max = words.length; i < max; ++i) {
+                const MACRO = words[i];
+
+                question = question.replace(
+                    MACRO,
+                    utils.getOneOfWord(MACRO)
+                );
+            }
+
+            // Заменя макрото за произволни цифри в обхват с произволно цяло число в този обхват
+            for (let i = 0, max = int_range.length; i < max; ++i) {
+                const MACRO = int_range[i];
+
+                question = question.replace(
+                    MACRO,
+                    utils.generateRandomIntegerFromString(MACRO)
+                );
+            }
+
+            // Заменя макрото за произволни цифри в обхват с произволно десетично число в този обхват
+            for (let i = 0, max = float_range.length; i < max; ++i) {
+                const MACRO = float_range[i];
+
+                question = question.replace(
+                    MACRO,
+                    utils.generateRandomFloatFromString(MACRO)
+                );
+            }
+
+            // Заменя макрото за случайно съществително с дума
+            for (let i = 0, max = nouns.length; i < max; ++i) {
+                const MACRO = nouns[i];
+
+                question = question.replace(
+                    MACRO,
+                    utils.getRandomNoun()
+                );
+            }
+
+            // Заменя макрото за произволно прилагателно с дума
+            for (let i = 0, max = adjectives.length; i < max; ++i) {
+                const MACRO = adjectives[i];
+
+                question = question.replace(
+                    MACRO,
+                    utils.getRandomAdjective()
+                );
+            }
+
+            // Завърших генерирането на j-тият въпрос
+            questions.push(question);
+        }
+
+        console.log(JSON.stringify(questions, null, 4));
+    });
+}
