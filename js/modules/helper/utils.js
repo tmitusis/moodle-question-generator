7ee8f5d7e6f3033b4daf1715423ae28dfbadfab6
tmitusis <tmitusis@gmail.com>
2023-03-11 15:22:31 +0200

    helper::utils.js: Some updates
    - Добавих брой цифри към генерирането на
    произволно десетично число
    - Добавих функциия и регулярен израз за работа със списъкът с прилагателни

--- js/modules/helper/utils.js
+++ js/modules/helper/utils.js
@@ -11,19 +11,34 @@ const utils = (function () {
         return generateRandomInteger(parseInt(range[0]), parseInt(range[1]));
     }
 
-    function generateRandomFloat(from, to) {
-        return (Math.random() * (to - from)) + from;
+    function generateRandomFloat(from, to, digits) {
+        return parseFloat(((Math.random() * (to - from)) + from).toFixed(digits));
     }
 
-    const FLOAT_RANGE_REGEX = /\[-?(\d+(\.\d+))\.\.-?(\d+(\.\d+))\]/gi;
+    const FLOAT_RANGE_REGEX = /\[-?(\d+(\.\d+))\.\.-?(\d+(\.\d+))\](@\d+)?/gi;
     function generateRandomFloatFromString(str) {
-        const range = str.replace(/[\[\]]/g, '').split('..')
-        return generateRandomFloat(parseFloat(range[0]), parseFloat(range[1]));
+        const at_idx = str.indexOf('@');
+        const range = str.replace(/[\[\]]/g, '')
+            .substring(0, at_idx === -1 ? str.length : at_idx)
+            .split('..')
+        ;
+        let digits = 15;
+
+        if (at_idx !== -1) {
+            digits = parseInt(str.substring(at_idx + 1, str.length));
+        }
+
+        return generateRandomFloat(parseFloat(range[0]), parseFloat(range[1]), digits);
     }
 
     const RANDOM_NOUN_REGEX = /\(съществително\)/gi;
     function getRandomNoun() {
         return nouns[generateRandomInteger(0, nouns.length - 1)];
+    }
+
+    const RANDOM_ADJECTIVE_REGEX = /\(прилагателно\)/gi;
+    function getRandomAdjective() {
+        return adjectives[generateRandomInteger(0, adjectives.length - 1)];
     }
 
     const ONEOF_WORD_REGEX = /{([а-я]*[a-z]*)(,[а-я]*[a-z]*)*}/gi;
@@ -39,6 +54,7 @@ const utils = (function () {
         INT_RANGE_REGEX,
         FLOAT_RANGE_REGEX,
         RANDOM_NOUN_REGEX,
+        RANDOM_ADJECTIVE_REGEX,
         ONEOF_WORD_REGEX,
 
         generateRandomInteger,
@@ -46,6 +62,7 @@ const utils = (function () {
         generateRandomFloat,
         generateRandomFloatFromString,
         getRandomNoun,
+        getRandomAdjective,
         getOneOfWord
     });
 })();
